8c8e08ae91bf91aa731dcc6afd9588aea2d8e079
Mohamed <mohamedabuali97@gmail.com>
2026-04-09 18:45:25 -0500

    add: added middleware in the backend

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,18 @@ import logging
 
 logger = logging.getLogger(__name__)
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 
 @app.get("/")
